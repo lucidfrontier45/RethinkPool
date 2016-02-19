10e90304f71f02a5754f2efaad1f75eeba5526f0
Shiqiao Du <lucidfrontier.45@gmail.com>
2016-02-19 17:06:30 +0900

    documents

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# RethinkPool
-RethinkDB Connection Pool for Python
+# RethinkDB Connection Pool for Python
+
+`rethinkpool` is a Python library for pooling RethinkDB connections.
+This is usefull for multi-thread application since a connection object Python driver provides is not thread-safe
+
+## Installation
+
+```bash
+$ pip install rethinkpool
+```
+
+## Usage
+
+Connection Pool can be created by instantiating `RethinkPool`.
+
+```python
+from rethinkpool import RethinkPool
+
+pool = RethinkPool()
+```
+
+The arguments for `RethinkPool` are as follows.
+
+- max_conn: maximum number of connections
+- get_timeout: timeout for obtaining a connection from the queue
+- host, port, ...: same as `r.connect`
+
+The pooled `ConnectionResource` object can be obtained by `pool.get_connection()`
+Used connection resource can be returned to the pool as follows.
+
+```python
+res = pool.get_connection()
+cur = r.table("data").run(res.conn)
+for obj in cur:
+    print(obj)
+res.release()
+```
+
+
+It also supports `with` statement for automatically restore used connection back to the pool.
+
+```python
+with pool.get_connection() as res:
+    cur = r.table("data").run(res.conn)
+    for obj in cur:
+        print(obj)
+```
--- rethinkpool/__init__.py
+++ rethinkpool/__init__.py
@@ -20,7 +20,9 @@ class ConnectionResource(object):
         return self._conn
 
     def release(self):
-        self._queue.put_nowait(self._conn)
+        if self._conn:
+            self._queue.put_nowait(self._conn)
+            self._conn = None
 
     def __enter__(self):
         return self
@@ -28,11 +30,19 @@ class ConnectionResource(object):
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.release()
 
+    def __del__(self):
+        self.release()
+
 
 class RethinkPool(object):
     def __init__(self, max_conn=10, get_timeout=10, host='localhost', port=28015, db=None, auth_key="", timeout=20,
-                 ssl=None,
-                 **kwargs):
+                 ssl=None, **kwargs):
+        """
+        :param max_conn: maximum number of connections
+        :param get_timeout: timeout for obtaining a connection from the queue
+        :param host, port, ...: same as r.connect
+        """
+
         self.get_timeout = get_timeout
         if ssl is None:
             ssl = dict()
@@ -42,16 +52,20 @@ class RethinkPool(object):
         for _ in range(max_conn):
             self._ready_conns.put(r.connect(host, port, db, auth_key, timeout, ssl, **kwargs))
 
-        self._internal_thread = threading.Thread(target=self.prepare_conns)
+        self._internal_thread = threading.Thread(target=self._prepare_conns)
         self._internal_thread.setDaemon(True)
         self._internal_thread.start()
 
-    def prepare_conns(self):
+    def _prepare_conns(self):
         while True:
             conn = self._uncleaned_conns.get()
             conn.reconnect(noreply_wait=False)
             self._ready_conns.put(conn)
 
     def get_connection(self):
+        """
+        obtain a connection resource from the queue
+        :return: ConnectionResource object
+        """
         conn = self._ready_conns.get(True, self.get_timeout)
         return ConnectionResource(self._uncleaned_conns, conn)
