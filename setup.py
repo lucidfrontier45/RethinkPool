091f1cc51fe6db564cc198fcde1bc63d267d218c
Shiqiao Du <lucidfrontier.45@gmail.com>
2016-02-22 14:22:40 +0900

    fixed setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ except ImportError:
 
 setup(
     name='RethinkPool',
-    version='0.1',
+    version='0.1.4',
     packages=['rethinkpool'],
     url='https://github.com/lucidfrontier45/RethinkPool',
     license='Apache License v2',
     author='Shiqiao Du',
     author_email='lucidfrontier.45@gmail.com',
     description='RethinkDB Connection Pool for Python',
-    requires=["future", "rethinkdb"]
+    install_requires=["future", "rethinkdb"]
 )
